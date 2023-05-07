f184cfb1d475216944fd8b4606570234e015e997
Александр <a.o.engovatov@yandex.ru>
2023-05-07 19:40:50 +0400

    добавлена аннотация @Size

--- ewm-main/src/main/java/ru/practicum/compilations/dto/CompilationUpdateDto.java
+++ ewm-main/src/main/java/ru/practicum/compilations/dto/CompilationUpdateDto.java
@@ -2,17 +2,18 @@ package ru.practicum.compilations.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class CompilationUpdateDto {
 
     private Boolean pinned;
 
+    @Size(max = 255)
     private String title;
 
     private List<Long> events;

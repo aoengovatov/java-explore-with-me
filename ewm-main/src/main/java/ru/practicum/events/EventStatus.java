954e084714a630f44bb37f656c50e9bb031a7b04
Александр <a.o.engovatov@yandex.ru>
2023-05-07 21:03:29 +0400

    исправление списка статусов

--- ewm-main/src/main/java/ru/practicum/events/EventStatus.java
+++ ewm-main/src/main/java/ru/practicum/events/EventStatus.java
@@ -2,10 +2,6 @@ package ru.practicum.events;
 
 public enum EventStatus {
     PENDING,
-    PUBLISH_EVENT,
-    CANCEL_REVIEW,
     PUBLISHED,
-    REJECT_EVENT,
-    SEND_TO_REVIEW,
     CANCELED;
 }

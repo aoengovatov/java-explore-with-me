03a50f5571c5664add1216a64a53374205d66397
Александр <a.o.engovatov@yandex.ru>
2023-05-08 10:32:23 +0400

    рефакторинг AdminController в EventAdminController

--- ewm-main/src/main/java/ru/practicum/events/controller/EventAdminController.java
+++ ewm-main/src/main/java/ru/practicum/events/controller/EventAdminController.java
@@ -1,52 +1,29 @@
-package ru.practicum.admin;
+package ru.practicum.events.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.events.EventService;
 import ru.practicum.events.EventStatus;
 import ru.practicum.events.dto.EventDto;
 import ru.practicum.events.dto.EventUpdateDto;
-import ru.practicum.users.UserService;
-import ru.practicum.users.dto.UserCreateDto;
-import ru.practicum.users.dto.UserDto;
 
-import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/events")
 @RequiredArgsConstructor
-public class AdminController {
+@Validated
+public class EventAdminController {
 
     @Autowired
-    private final UserService userService;
     private final EventService eventService;
 
-    @GetMapping("/users")
-    public List<UserDto> getUsers(@RequestParam(name = "ids", defaultValue = "ALL") List<Long> ids,
-                                  @PositiveOrZero @RequestParam (name = "from", defaultValue = "0") Integer from,
-                                  @Positive @RequestParam (name = "size", defaultValue = "10") Integer size) {
-        return userService.getUsers(ids, from, size);
-    }
-
-    @PostMapping("/users")
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserDto addUser(@Valid @RequestBody UserCreateDto dto) {
-        return userService.add(dto);
-    }
-
-    @DeleteMapping("/users/{userId}")
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long userId) {
-        userService.delete(userId);
-    }
-
-    @GetMapping("/events")
+    @GetMapping
     public List<EventDto> searchEvents(@RequestParam(value = "users", required = false) List<Long> userIds,
                                        @RequestParam(value = "states", required = false) List<EventStatus> states,
                                        @RequestParam(value = "categories", required = false) List<Long> categories,
@@ -57,7 +34,7 @@ public class AdminController {
         return eventService.getEventsWithFilters(userIds, states, categories, rangeStart, rangeEnd, from, size);
     }
 
-    @PatchMapping("/events/{eventId}")
+    @PatchMapping("/{eventId}")
     public EventDto updateEventById(@PathVariable Long eventId,
                                     @RequestBody EventUpdateDto dto) {
         return eventService.updateFromAdmin(eventId, dto);
